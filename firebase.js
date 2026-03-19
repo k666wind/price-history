6924791b2f8ae56fe005cb29c0d50b2bd541ac94
k666wind <k666wind@gmail.com>
2026-03-19 22:06:50 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // Import the functions you need from the SDKs you need
 //import { initializeApp } from "firebase/app";
@@ -22,6 +21,4 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
-export const auth = getAuth(app);
