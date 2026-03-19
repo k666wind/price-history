231ff6865a346f2126962755bf8e512aae8a376c
k666wind <k666wind@gmail.com>
2026-03-19 22:07:23 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,9 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('v1').then(cache => cache.addAll([
-      '/', '/index.html', '/style.css', '/app.js'
-    ]))
+    caches.open('v1').then(cache => cache.addAll(['/', '/index.html', '/style.css', '/app.js']))
   );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
