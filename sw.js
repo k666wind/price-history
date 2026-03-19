ff6669c96f803a9df151de01f5d4dc7ea675d404
k666wind <k666wind@gmail.com>
2026-03-19 22:00:23 +0000

    Update sw.js
    V2-withScan

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('v1').then(cache => cache.addAll([
-      '/',
-      '/index.html',
-      '/style.css',
-      '/app.js'
+      '/', '/index.html', '/style.css', '/app.js'
     ]))
   );
 });
