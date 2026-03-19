671738a3e5f1ed23684dbe8897f4a142a86b1884
k666wind <k666wind@gmail.com>
2026-03-19 20:28:34 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('app-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html'
-      ]);
-    })
+    caches.open('v1').then(cache => cache.addAll([
+      '/',
+      '/index.html',
+      '/style.css',
+      '/app.js'
+    ]))
   );
 });
 
