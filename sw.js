b1c4c3dfe38f0f5f5174eabe1ebc8dafc62500be
k666wind <k666wind@gmail.com>
2026-03-19 23:18:31 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,24 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('v1').then(cache => cache.addAll(['/', '/index.html', '/style.css', '/app.js']))
+const CACHE_NAME = 'shopping-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/firebase.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache.map(url => new Request(url, {cache: 'reload'}))))
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(()=> fetch(event.request)) // fallback
+  );
 });
